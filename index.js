5728464c550dbb6858be3fed8839d0b707c073f1
ibr <ibr.seckin@gmail.com>
2024-07-17 06:53:33 +0300

    cors link

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require('./src/config/dbConnection')();
 
 //?------#- MIDDLEWARES -#------
 app.use(express.json());
-app.use(cors({origin:'http://localhost:3000'}));
+app.use(cors({origin:'https://tutortodo-app-backend.vercel.app'}));
 
 
 //?------#- ROUTES -#------
